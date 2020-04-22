b9ac1ac7786c8a5bc7ae40455f760cd3f92b735c
Anton <anton.matrenin@introduct.tech>
2020-04-23 00:30:31 +0300

    add repo

--- lib/domain/core/validations.dart
+++ lib/domain/core/validations.dart
@@ -2,15 +2,19 @@ import 'package:dartz/dartz.dart';
 import 'package:web_form/domain/core/value_failure.dart';
 
 abstract class IValidator<T> {
-  Option<ValueFailure> validate(T value);
+  Option<ValueFailure<T>> validate(T value);
 }
 
-Either<ValueFailure<T>, T> validate<T>(value, List<IValidator> validators) {
-  var checkedValidators = validators.map((e) => e.validate(value));
+Either<ValueFailure<T>, T> validate<T>(
+  T value,
+  List<IValidator<T>> validators,
+) {
+  final checkedValidators = validators.map((e) => e.validate(value));
 
-  Option<ValueFailure<T>> activeFailure = checkedValidators.firstWhere(
-      (element) => element.isSome(),
-      orElse: () => none<ValueFailure<int>>());
+  final activeFailure = checkedValidators.firstWhere(
+    (element) => element.isSome(),
+    orElse: () => none<ValueFailure<T>>(),
+  );
 
   return activeFailure.fold(
       () => right(value), (valueFailure) => left(valueFailure));
